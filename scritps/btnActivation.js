4c890c42fd3fae11af86bc035cb3f9a6db674eed
ChoYoungHo <choyoungho95@gmail.com>
2020-09-29 16:23:33 +0900

    login 영역 input label 완료

---
+++ scritps/btnActivation.js
@@ -0,0 +1,4 @@
+// let textValue = document.querySelectorAll("input[type='text']");
+// let pwdValue = document.querySelectorAll("input[type='password']");
+// let numberValue = document.querySelectorAll("input[type='number']");
+// console.log(textValue, pwdValue, numberValue);
