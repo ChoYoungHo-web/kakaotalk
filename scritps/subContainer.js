ef4755946ddc529c3ddb7790e283fd49dedd5c0c
ChoYoungHo <choyoungho95@gmail.com>
2020-10-07 14:44:04 +0900

    index, chatting 페이지 js

--- scritps/subContainer.js
+++ scritps/subContainer.js
@@ -2,45 +2,27 @@ const CLICK = document.querySelector("#subClickBtn");
 
 let body = document.querySelector("body");
 let main = document.querySelector("main");
+
 let friendAdd = document.querySelector('[name="friendAdd"]');
 let createChatting = document.querySelector('[name="createChatting"]');
 
 let createSub = document.querySelector("#subContainer");
+let popup = document.querySelector("#popup");
 
-let friendAddStyle = (v) => {
-  if (v.classList[4] === "active") {
-    body.classList.remove("body__sub--background-color");
-    main.classList.remove("main__sub--over-flow");
-    v.classList.remove("active");
-    createSub.classList.add("none");
-    createSub.classList.remove("sub-container--create", "sub-friend");
-  } else {
-    body.classList.add("body__sub--background-color");
-    main.classList.add("main__sub--over-flow");
-    v.classList.add("active");
-    createSub.classList.remove("none");
-    createSub.classList.add("sub-container--create", "sub-friend");
-  }
-};
-let crateChattingStyle = (v) => {
-  if (v.classList[4] === "active") {
-    v.classList.remove("active");
-    createSub.classList.add("none");
-    createSub.classList.remove("sub-container--create", "sub-chatting");
-  } else {
-    v.classList.add("active");
-    createSub.classList.remove("none");
-    createSub.classList.add("sub-container--create", "sub-chatting");
-  }
+popupCloseFunction = () => {
+  popup.classList.remove("sub-friend");
+  main.classList.remove("main__sub--over-flow");
+  createSub.classList.add("none");
+  createSub.classList.remove("sub-container--create");
 };
 
-let subContainer = () => {
-  if (friendAdd) {
-    friendAddStyle(friendAdd);
-  }
-  if (createChatting) {
-    crateChattingStyle(createChatting);
-  }
+let friendAddStyle = () => {
+  let popupClose = document.querySelector("#popupClose");
+  popup.classList.add("popup-container", "sub-friend");
+  main.classList.add("main__sub--over-flow");
+  createSub.classList.remove("none");
+  createSub.classList.add("sub-container--create");
+  popupClose.addEventListener("click", popupCloseFunction);
 };
 
-CLICK.addEventListener("click", subContainer);
+CLICK.addEventListener("click", friendAddStyle);
